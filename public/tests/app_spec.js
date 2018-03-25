cba17e9db1a542aa2cf310bd1695a309a4925d16
tmd45 <tmd45@otegami.jpn.org>
2018-03-25 21:43:38 +0900

    :book: 3.3 Handling User Input

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -30,7 +30,6 @@ describe('LearnJS', function () {
 
   describe('problem view', function () {
     var view;
-
     beforeEach(function () {
       view = learnjs.problemView('1');
     });
@@ -45,6 +44,21 @@ describe('LearnJS', function () {
 
     it('shows the problem code', function () {
       expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
+    });
+
+    describe('answer section', function () {
+      it('can check a correct answer by hitting a button', function () {
+        // <textarea> の値を設定してから、ボタンのクリックイベントをトリガーする
+        view.find('.answer').val('true');
+        view.find('.check-btn').click();
+        expect(view.find('.result').text()).toEqual('Correct!');
+      });
+
+      it('rejects an incorrect answer', function () {
+        view.find('.answer').val('false');
+        view.find('.check-btn').click();
+        expect(view.find('.result').text()).toEqual('Incorrect!');
+      });
     });
   });
 });
